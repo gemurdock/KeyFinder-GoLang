721c9e5975d70ce4e977d7a1bae4548ef3053949
Glen Murdock <gemurdock@gmail.com>
2023-12-12 00:11:06 -0500

    Added chi router; hello world working locally

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now()
+		fmt.Println("The current time is: ", currentTime, " - Hello World sent to the browser")
+		w.Write([]byte("Hello World"))
+	})
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", r)
 }
