114e8e5bbe4c0eef8dbb0ec96962ab84020ab50c
Glen Murdock <gemurdock@gmail.com>
2023-12-14 00:21:25 -0500

    Fixed User GORM structure

--- db/model/user.go
+++ db/model/user.go
@@ -8,9 +8,10 @@ import (
 
 type User struct {
 	gorm.Model
-	Username  string    `json:"username" gorm:"unique_index;varchar(25);not null"`
-	Password  string    `json:"password" gorm:"varchar(255);not null"`
-	CreatedAt time.Time `json:"created" gorm:"type:timestamptz; not null"`
-	DeletedAt time.Time `json:"deleted" gorm:"type:timestamptz"`
-	LastLogin time.Time `json:"last_login" gorm:"type:timestamptz"`
+	Username  string         `json:"username" gorm:"unique_index;varchar(25);not null"`
+	Password  string         `json:"password" gorm:"varchar(255);not null"`
+	LastLogin time.Time      `json:"last_login" gorm:"type:timestamptz"`
+	CreatedAt time.Time      `json:"created" gorm:"type:timestamptz; not null"`
+	UpdatedAt time.Time      `json:"updated" gorm:"type:timestamptz; not null"`
+	DeletedAt gorm.DeletedAt `json:"deleted" gorm:"index"`
 }
